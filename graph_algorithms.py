d56c6c130b5aed36f017ca80cf670a39da32bd22
mnursey <mitchnursey@gmail.com>
2018-02-05 23:35:24 -0800

    Updated Pathfinding

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -3,12 +3,41 @@ def distance(a, b):
     dy = abs(a[1] - b[1])
     return dx+dy
 
+
+def direction(a, b):
+    if(a[0] > b[0]):
+        return 'left'
+    if(a[0] < b[0]):
+        return 'right'
+    if(a[1] > b[1]):
+        return 'up'
+    if(a[1] < b[1]):
+        return 'down'
+
+
+def get_diagonals(node, grid, ignore_list):
+    width = len(grid)
+    height = len(grid[0])
+    result = []
+    if(node[0] > 0 and node[1] > 0):
+        result.append((node[0]-1, node[1]-1))
+    if(node[0] < width - 1 and node[1] < height - 1):
+        result.append((node[0]+1, node[1]+1))
+    if(node[0] < width - 1 and node[1] > 0):
+        result.append((node[0]+1, node[1]-1))
+    if(node[0] > 0 and node[1] < height - 1):
+        result.append((node[0]-1, node[1]+1))
+    result = filter(lambda n: (grid[n[0]][n[1]] not in ignore_list), result)
+    open_set = []
+    for r in result:
+        open_set.append(r)
+    return open_set
+
+
 def neighbours(node, grid, ignore_list):
     width = len(grid)
     height = len(grid[0])
-    
     result = []
-    #self, pos, value, start, goa
     if(node[0] > 0):
         result.append((node[0]-1, node[1]))
     if(node[0] < width - 1):
@@ -17,59 +46,269 @@ def neighbours(node, grid, ignore_list):
         result.append((node[0], node[1]-1))
     if(node[1] < height-1):
         result.append((node[0], node[1]+1))
-	
-    result = filter(lambda n: (grid[n[0]][n[1]] not in ignore_list), result) 
-	
-    return result
+    result = filter(lambda n: (grid[n[0]][n[1]] not in ignore_list), result)
+    open_set = []
+    for r in result:
+        open_set.append(r)
+    return open_set
+
+
+def on_edge_of_grid(node, grid):
+    width = len(grid)
+    height = len(grid[0])
+    if(node[0] == 0):
+        return True
+    if(node[0] == width - 1):
+        return True
+    if(node[1] == 0):
+        return True
+    if(node[1] == height-1):
+        return True
+
+    return False
+
 
 def trace_path(came_from, current):
     result = [current]
-	
+
     while current in came_from.keys():
         current = came_from[current]
         result.append(current)
-    
+
     result.reverse()
     return result
 
-def find_path(start, goal, grid, ignore_list):
+
+def find_path(start, goal, waypoints, links, grid, obstacles):
     start = tuple(start)
     goal = tuple(goal)
-	
+    grid[start[0]][start[1]] = 0
+    links[start] = connecting_points(start, waypoints, grid, obstacles)
+    connect_points_to(goal, waypoints, links, grid, obstacles)
+    connect_point_to(start, goal, links, grid, obstacles)
+    ''' Make Waypoints point to the goal!'''
     open_set = [start]
     closed_set = []
     came_from = {}
 
-    g_score = [[10000 for x in range(len(grid[y]))] for y in range(len(grid))]
-    g_score[start[0]][start[1]] = 0
-    
-    f_score = [[10000 for x in range(len(grid[y]))] for y in range(len(grid))]
-    f_score[start[0]][start[1]] = distance(start, goal)
-        
+    g_score = {}
+    for w in waypoints:
+        g_score[w] = 10000
+    g_score[start] = 0
+
+    f_score = {}
+    for w in waypoints:
+        f_score[w] = 10000
+    f_score[start] = distance(start, goal)
+
     while len(open_set) > 0:
-        current = min(open_set, key=lambda p: f_score[p[0]][p[1]])
-        
+        # print(len(open_set))
+        # fix... adapt for waypoints
+        current = open_set[0]
+        for w in open_set:
+            # print(w)
+            if(f_score[w] < f_score[current]):
+                w = current
+
         if(current == goal):
             path = trace_path(came_from, current)
-            #for x in path:
-            #    print(str(x))
+            grid[start[0]][start[1]] = 2
+            for w in waypoints:
+                if goal in links[w]:
+                    links[w].remove(goal)
+            '''for x in path:
+                print(str(x))'''
             return path
 
         open_set.remove(current)
         closed_set.append(current)
-                      
-        for n in neighbours(current, grid, ignore_list):
+
+        for n in links[current]:
             if n in closed_set:
                 continue
-                      
-            tentative_g_score = g_score[current[0]][current[1]] + distance(current, n)
-                      
+
+            tentative_g_score = g_score[current] + distance(current, n)
+
             if n not in open_set:
-                open_set.append(n)      
-            elif tentative_g_score >= g_score[n[0]][n[1]]:
+                open_set.append(n)
+            elif tentative_g_score >= g_score[n]:
                 continue
 
             came_from[n] = current
-            g_score[n[0]][n[1]] = tentative_g_score
-            f_score[n[0]][n[1]] = tentative_g_score + distance(n, goal)         
+            g_score[n] = tentative_g_score
+            f_score[n] = tentative_g_score + distance(n, goal)
     return None
+
+
+def generate_waypoints(grid, obstacles):
+    '''Fix waypoint placement, also include waypoints around the head.
+    A few more waypoints would be better than a broken path'''
+    waypoints = []
+    generated_points = []
+    # width = len(grid)
+    # height = len(grid[0])
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] in obstacles:
+                pos = (x, y)
+                dia = get_diagonals(pos, grid, obstacles)
+                for d in dia:
+                    neigh = neighbours(d, grid, [])
+                    add = True
+                    for n in neigh:
+                        if grid[n[0]][n[1]] in obstacles:
+                            add = False
+                            break
+                    if d not in generated_points and add:
+                        generated_points.append(d)
+                        waypoints.append(d)
+                neigh = neighbours(pos, grid, [1, 2, 4])
+                if len(neigh) == 2:
+                    if neigh[0][0] != neigh[1][0] and neigh[0][1] != neigh[1][1]:
+                        for n in neigh:
+                            if n not in generated_points:
+                                generated_points.append(n)
+                                waypoints.append(n)
+                if on_edge_of_grid(pos, grid):
+                    for n in neigh:
+                        if n not in generated_points:
+                            generated_points.append(n)
+                            waypoints.append(n)
+    '''for w in waypoints:
+        grid[w[0]][w[1]] = 'W'
+        # print(w)
+    display_grid(grid)'''
+    return waypoints
+
+
+def link_waypoints(waypoints, grid, obstacles):
+    links = {}
+    for a in waypoints:
+        results = []
+        for b in waypoints:
+            if a is b:
+                continue
+            x = min([a[0], b[0]])
+            y = min([a[1], b[1]])
+            x_max = max([a[0], b[0]])
+            y_max = max([a[1], b[1]])
+            # print(str(x) + '->' + str(x_max) + ' ' + str(y) + '->' + str(y_max))
+            add = True
+            while x <= x_max:
+                y = min([a[1], b[1]])
+                while y <= y_max:
+                    # print(str(x) + ':' + str(y) + ' value:' + str(grid[x][y]))
+                    if grid[x][y] in obstacles:
+                        # print('Something in the way from linking' + str(a) + ' ' + str(b))
+                        add = False
+                        break
+                    y = y + 1
+                x = x + 1
+                if not add:
+                    break
+            if add:
+                # print(a)
+                # print(b)
+                # print('Connected two waypoints')
+                results.append(b)
+        links[a] = results
+
+    return links
+
+
+def connecting_points(target, waypoints, grid, obstacles):
+    results = []
+    a = target
+    for b in waypoints:
+        if a is b:
+            continue
+        x = min([a[0], b[0]])
+        y = min([a[1], b[1]])
+        x_max = max([a[0], b[0]])
+        y_max = max([a[1], b[1]])
+        # print(str(x) + '->' + str(x_max) + ' ' + str(y) + '->' + str(y_max))
+        add = True
+        while x <= x_max:
+            y = min([a[1], b[1]])
+            while y <= y_max:
+                # print(str(x) + ':' + str(y) + ' value:' + str(grid[x][y]))
+                if grid[x][y] in obstacles:
+                    # print('Something in the way from linking' + str(a) + ' ' + str(b))
+                    add = False
+                    break
+                y = y + 1
+            x = x + 1
+            if not add:
+                break
+        if add:
+            # print(a)
+            # print(b)
+            # print('Connected two waypoints')
+            results.append(b)
+
+    return results
+
+
+def connect_points_to(target, waypoints, links, grid, obstacles):
+    a = target
+    for b in waypoints:
+        if a is b:
+            continue
+        x = min([a[0], b[0]])
+        y = min([a[1], b[1]])
+        x_max = max([a[0], b[0]])
+        y_max = max([a[1], b[1]])
+        # print(str(x) + '->' + str(x_max) + ' ' + str(y) + '->' + str(y_max))
+        add = True
+        while x <= x_max:
+            y = min([a[1], b[1]])
+            while y <= y_max:
+                # print(str(x) + ':' + str(y) + ' value:' + str(grid[x][y]))
+                if grid[x][y] in obstacles:
+                    # print('Something in the way from linking' + str(a) + ' ' + str(b))
+                    add = False
+                    break
+                y = y + 1
+            x = x + 1
+            if not add:
+                break
+        if add:
+            links[b].append(target)
+            # print(a)
+            # print(b)
+            # print('Connected two waypoints')
+
+
+def connect_point_to(start, target, links, grid, obstacles):
+    b = start
+    a = target
+    if a is b:
+        return
+    x = min([a[0], b[0]])
+    y = min([a[1], b[1]])
+    x_max = max([a[0], b[0]])
+    y_max = max([a[1], b[1]])
+    # print(str(x) + '->' + str(x_max) + ' ' + str(y) + '->' + str(y_max))
+    add = True
+    while x <= x_max:
+        y = min([a[1], b[1]])
+        while y <= y_max:
+            # print(str(x) + ':' + str(y) + ' value:' + str(grid[x][y]))
+            if grid[x][y] in obstacles:
+                # print('Something in the way from linking' + str(a) + ' ' + str(b))
+                add = False
+                break
+            y = y + 1
+        x = x + 1
+        if not add:
+            break
+    if add:
+        links[b].append(target)
+
+
+def display_grid(grid):
+    for y in range(len(grid)):
+        row = ""
+        for x in range(len(grid[y])):
+            row = row + str(grid[x][y]) + " "
+        print(row)
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from graph_algorithms import find_path
+from graph_algorithms import find_path, generate_waypoints, link_waypoints
 import bottle
 import os
-import random
-import copy
-import math
 import time
 
 EMPTY = 0
@@ -14,9 +11,11 @@ DANGER = 4
 DIRECTIONS = ['up', 'down', 'left', 'right']
 taunt = 'Do battle snakes dream of electric apples?'
 
+
 def point_to_list(json_object):
     return (json_object['x'], json_object['y'])
-    
+
+
 def objectives(data):
     results = []
     food = data['food']['data']
@@ -24,34 +23,37 @@ def objectives(data):
         results.append(point_to_list(f))
     return results
 
+
 def display_grid(grid):
     for y in range(len(grid)):
         row = ""
         for x in range(len(grid[y])):
-            row = row +  str(grid[x][y]) + " "
+            row = row + str(grid[x][y]) + " "
         print(row)
-        
-def generate_grid(snake_id, my_snake_length,data):
+
+
+def generate_grid(snake_id, my_snake_length, data):
     grid = [[0 for col in range(data['height'])] for row in range(data['width'])]
-    
+
     for food in data['food']['data']:
         food = point_to_list(food)
         grid[food[0]][food[1]] = FOOD
-        
+
     for snake in data['snakes']['data']:
         for coord in snake['body']['data']:
             coord = point_to_list(coord)
-            #Add in once accounting for eating an apple
-            #if coord != snake['coords'][-1]: 
+            # Add in once accounting for eating an apple
+            # if coord != snake['coords'][-1]:
             grid[coord[0]][coord[1]] = SNAKE
-        
+
         if snake_id != snake['id']:
             if my_snake_length <= snake['length']:
-                danger_spots = open_neighbours(point_to_list(snake['body']['data'][0]),grid)
+                danger_spots = open_neighbours(point_to_list(snake['body']['data'][0]), grid)
                 for n in danger_spots:
                     grid[n[0]][n[1]] = DANGER
 
     return grid
+
 
 def direction(a, b):
     if(a[0] > b[0]):
@@ -63,40 +65,55 @@ def direction(a, b):
     if(a[1] < b[1]):
         return 'down'
 
+
+def smart_direction(a, b, grid, obstacles):
+    if(a[0] > b[0] and grid[a[0] - 1][a[1]] not in obstacles):
+        return 'left'
+    if(a[0] < b[0] and grid[a[0] + 1][a[1]] not in obstacles):
+        return 'right'
+    if(a[1] > b[1] and grid[a[0]][a[1] - 1] not in obstacles):
+        return 'up'
+    if(a[1] < b[1] and grid[a[0]][a[1] + 1] not in obstacles):
+        return 'down'
+
+
 def distance(a, b):
     dx = abs(a[0] - b[0])
     dy = abs(a[1] - b[1])
     return dx+dy
 
-def is_free(target,grid):
+
+def is_free(target, grid):
     value = grid[target[0]][target[1]]
     if(value == 1 or value == 4 or value == 2):
         return False
     return True
-    
+
+
 def open_neighbours(pos, grid):
     width = len(grid)
     height = len(grid[0])
-    
+
     result = []
     if(pos[0] > 0):
-        n = (pos[0]-1,pos[1])
+        n = (pos[0]-1, pos[1])
         if(is_free(n, grid)):
             result.append(n)
     if(pos[0] < width - 1):
-        n = (pos[0]+1,pos[1])
+        n = (pos[0]+1, pos[1])
         if(is_free(n, grid)):
             result.append(n)
     if(pos[1] > 0):
-        n = (pos[0],pos[1]-1)
+        n = (pos[0], pos[1]-1)
         if(is_free(n, grid)):
             result.append(n)
     if(pos[1] < height-1):
-        n = (pos[0],pos[1]+1)
+        n = (pos[0], pos[1]+1)
         if(is_free(n, grid)):
             result.append(n)
 
     return result
+
 
 def run_ai(data):
     move = 'left'
@@ -104,35 +121,51 @@ def run_ai(data):
     goals = objectives(data)
     my_snake_length = data['you']['length']
     my_snake_health = data['you']['health']
+    if my_snake_health < 15:
+        print('About to die of hunger!')
+    grid = generate_grid(snake_id, my_snake_length, data)
 
-    grid = generate_grid(snake_id, my_snake_length,data)
-    
     my_snake_head = point_to_list(data['you']['body']['data'][0])
-    current_goal = None
+    # current_goal = None
     current_path = None
-    
+    start = time.time()
+    waypoints = generate_waypoints(grid, [1, 2, 4])
+    end = time.time()
+    print('Time to waypoints: ' + str((end - start) * 1000) + 'ms')
+    start = time.time()
+    links = link_waypoints(waypoints, grid, [1, 2, 4])
+    '''for w in links:
+        for p in w:
+            print(str(p[0]) + ' ' + str(p[1]))
+        print('End Of Connected')'''
+    end = time.time()
+    print('Time to link waypoints: ' + str((end - start) * 1000) + 'ms')
     for goal in goals:
         start = time.time()
-        path = find_path(my_snake_head, goal, grid, [1,2,4])
+        path = find_path(my_snake_head, goal, waypoints, links, grid, [1, 2, 4])
         end = time.time()
         print('Time to get path from o_path: ' + str((end - start) * 1000) + 'ms')
         current_path = path
-        
-    if current_path != None: 
-        move = direction(my_snake_head, current_path[1])
+
+    if current_path is not None:
+        '''print('Printing path')
+        for n in current_path:
+            print(n)'''
+        move = smart_direction(my_snake_head, current_path[1], grid, [1, 2, 4])
         print('Going to ' + str(current_path[1]) + ' by going ' + str(move))
     else:
-        #print('No good goals!!!')
+        # print('No good goals!!!')
         possible_positions = open_neighbours(my_snake_head, grid)
         if len(possible_positions) > 0:
-            taunt = 'That was irrational of you. Not to mention unsportsmanlike.'
+            # taunt = 'That was irrational of you. Not to mention unsportsmanlike.'
             move = direction(my_snake_head, possible_positions[0])
         else:
             move = 'down'
-            taunt = 'That was irrational of you. Not to mention unsportsmanlike.'
+            # taunt = 'That was irrational of you. Not to mention unsportsmanlike.'
         print('Going ' + move)
 
     return move
+
 
 @bottle.post('/start')
 def start():
@@ -141,14 +174,14 @@ def start():
     print("New game started!")
     board_width = data['width']
     board_height = data['height']
-    #print(str(game_id) + " " + str(board_width) + " " + str(board_height))
+    print(str(game_id) + " " + str(board_width) + " " + str(board_height))
 
     # TODO: Do things with data
 
     return {
-        'color': 'gold',
+        'color': 'DarkMagenta',
         'secondary_color': 'red',
-        'taunt':' Quite an experience to live in fear, isn\'t it?',
+        'taunt': 'Quite an experience to live in fear, isn\'t it?',
         'name': 'Batty Snake'
     }
 
@@ -156,7 +189,7 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-    #print(str(data))
+    # print(str(data))
     start = time.time()
     output = run_ai(data)
     end = time.time()
@@ -166,12 +199,15 @@ def move():
         'taunt': taunt
     }
 
+
 @bottle.get('/')
 def status():
     return{
         "<!DOCKTYPE html><html><head><title>2018</title><style>p{color:orange;}</style></head><body><p>BattleSnake 2018 by Mitchell Nursey.</p></body></html>"
     }
+
+
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
-    bottle.run(application, server='auto',host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+    bottle.run(application, server='auto', host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
